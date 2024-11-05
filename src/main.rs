0acf2b44ac42f5389210875478cb30aa67ba03dd
Teun Visser <user.yukisnow@gmail.com>
2024-11-05 14:47:34 +0100

    using ~/.config now, code cleanup and RUST_LOG=3 (info) is setup

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,40 @@
-// use substring::Substring;
-use std::env;
+use std::{
+    env, 
+    fs::OpenOptions,
+    io::{prelude::*, BufReader}
+};
 use rand::seq::SliceRandom;
-use home::home_dir;
 
-// let mut Some 
+
 fn main() {
+    env_logger::init();
+
     let args: Vec<String> = env::args().collect();
-    // dbg!(args);
-    if let Some((w, h)) = term_size::dimensions() {
-        // print_type_of(&w);
-        println!("TODO!:\nThis window has a Width of {}\nuse this to center le dragon :3", w);    
+    if args.len() == 2 {
+        sayy(args[1].to_string())
     }
-        
-   
-        if args.len() > 1 { 
-        let query = &args[1];
-        let prompt = &args[2];
-        if query == "-scroll" {sayy(prompt.to_string())}
-        else {sayy("".to_string());}}
-    else {sayy("".to_string())}
-    // let input = &args[2];
+    else {
+        sayy(generate_string());
+    }
 
-    
-    // println!("|￣￣￣￣￣￣￣￣￣￣|\n| GOD MAY JUDGE YOU, |\n|   BUT HIS SINS     |\n| OUTNUMBER YOUR OWN |\n|＿＿＿＿＿＿＿＿＿＿|\n(\\__/) ||\n(•ㅅ•) || \n/ 　  づ\n");
-    // println!("{}", say);
-
-     
-    
-    // read();
 }
 
-fn print_type_of<T>(_: &T) {
-    println!("{}", std::any::type_name::<T>())
-}
+//fn print_type_of<T>(_: &T) { //new to Rust (as you can see), this block identifies type of object
+//    println!("{}", std::any::type_name::<T>())
+//}
 
-fn sayy(mut inp: String) {
-    let dragontop = r"                                  /   \
+fn sayy(inp: String) {
+    log::debug!("Displaying line: '{inp}'");
+    let padd = if let Some((w, _h)) = term_size::dimensions() {
+        (w - 73) / 2 //w is width, 73 is width of dragon o7
+    } else {
+        // Handle case where terminal dimensions are not available
+        log::debug!("Could not retrieve terminal dimensions.");
+        0 // Fallback padding value
+    };
+    let padding = " ".repeat(padd);
+    let dragontop = r"
+                                  /   \
                           )      ((   ))     (
 (@)                      /|\      ))_((     /|\
 |-|\.                   / | \    (/\|/\)   / | \                      (@)
@@ -43,7 +42,9 @@ fn sayy(mut inp: String) {
 |-|                         '^`   (o o)  '^`                          | |
 | |                               `\Y/'                               |-|
 | |                                                                   | |";
-    let dragonbot = r"| |                                                                   |-|
+    let dragonbot = 
+r"
+| |                                                                   |-|
 |_|___________________________________________________________________| |
 (@)                 I   /\ /      ( (       \ /\   I                `\|-|
                     I /   V        \ \       V   \ I                  (@)
@@ -51,43 +52,70 @@ fn sayy(mut inp: String) {
                                    _) )_ 
                                    `\ /'
 ";
-    let mut signpre = r"| |     ";
-    println!("{}", dragontop);
+    let signpre = r"| |     ";
     //
     // println!("{}, {}", generateString(), &generateString()[1..8]);
-    let mut text = Vec::<String>::new();
-    if inp != "" {
-        text = formatString(inp);
+    let mut text;// = Vec::<String>::new();
+    if !inp.is_empty() {
+        text = format_string(inp);
     } else {
-        text = formatString(generateString().to_string());
+        text = format_string(generate_string().to_string());
     }
 
-    let mut finalstr = String::from("Hello, world!");
     if text.len() == 1 {
         let v = text[0].len() as f32 / 2.0;
         text[0] = r" ".repeat(28 - v.floor() as usize) + &text[0][..];
         // signpre = &finalstr[..];
         // println!("{}", finalstr)
     }
-    for item in text {
-        let signpost = " ".repeat(62 - item.len());
-        println!("{}{}{}| |",signpre, item, signpost);
+
+    
+    //println!("{}", dragontop);
+    let dragontop: Vec<String> = dragontop.lines().skip(1)
+        .map(String::from)
+        .collect();
+    for line in dragontop.iter() {
+        println!("{padding}{line}", );
     }
 
-    println!("{}", dragonbot);
-}
-// extern crate random;
+    for item in text {
+        let signpost = " ".repeat(62 - item.len());
+        println!("{padding}{}{}{}| |",signpre, item, signpost);
+    }
+    
+    //println!("{}", dragonbot);
+    let dragonbot: Vec<String> = dragonbot.lines().skip(1)
+        .map(String::from)
+        .collect();
 
-fn generateString() -> String {
-    let home = home_dir().expect("READON").into_os_string().into_string().unwrap();
-    // println!("{}", home);
-    let options = lines_from_file(home + "/lines");
+    for line in dragonbot.iter() {
+        println!("{padding}{line}", );
+    }
+}
+
+fn generate_string() -> String {
+    let config = dirs::config_dir().expect("READON").into_os_string().into_string().unwrap() + "/dragonprint/lines";
+    //below this is all stupid
+    //try to read the lines file here
+    //if succeed, then pick a random line from it and return
+    //if fail, create the file with 'add lines to ~/.config/dragonprint/lines' in it
+    //re-run this function
+    let mut file = OpenOptions::new().read(true).write(true).create(true).open(config).expect("fuck");
+
+    let buf = BufReader::new(&file);
+    let options: Vec<String> = buf.lines()
+        .map(|l| l.expect("Could not parse line"))
+        .collect();
+
     // let mut rng = rand::thread_rng();
-    let pick = options.choose(&mut rand::thread_rng()).unwrap();
-    return pick.to_string()
+    let pick = match options.choose(&mut rand::thread_rng()) {
+        Some(entry) => entry.to_string(),
+        None => {let _ = file.write_all(b"Add dialogue to `~/.config/dragonprint/lines"); generate_string()},
+    };
+    pick
 }
 
-fn formatString(mut input: String) -> Vec<String> {
+fn format_string(mut input: String) -> Vec<String> {
     input = " ".to_owned() + &input;
     let mut vecc: Vec<String> = vec![];
     while input.len() > 60 {
@@ -102,6 +130,7 @@ fn formatString(mut input: String) -> Vec<String> {
     vecc.push(input[1..input.len()].to_string());
     return vecc;
 
+//TODO: I think?
 //start while loop 
 //start another while loop, that continues if char 60 is a whitespace
 //if not, do char - 1, if that is a whitespace, cut the string left of char and append to list
@@ -109,18 +138,6 @@ fn formatString(mut input: String) -> Vec<String> {
 }
 
 
-use std::{
-    fs::File,
-    io::{prelude::*, BufReader},
-    path::Path,
-};
 
-fn lines_from_file(filename: impl AsRef<Path>) -> Vec<String> {
-    let file = File::open(filename).expect("no such file (please put it in home)");
-    let buf = BufReader::new(file);
-    buf.lines()
-        .map(|l| l.expect("Could not parse line"))
-        .collect()
-}
 
 
